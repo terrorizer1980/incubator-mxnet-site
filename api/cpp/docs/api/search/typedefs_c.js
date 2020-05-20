7f118856b1911059bc2745a94e282d3a2c714648
mxnet-ci <mxnet-ci>
2020-05-20 00:42:57 +0000

    Publish triggered by CI

--- api/cpp/docs/api/search/typedefs_c.js
+++ api/cpp/docs/api/search/typedefs_c.js
@@ -6,5 +6,6 @@ var searchData=
   ['ndlisthandle',['NDListHandle',['../c__predict__api_8h.html#a3058aee7831edae29e9ac39556e5fe5c',1,'c_predict_api.h']]],
   ['nn_5fuint',['nn_uint',['../3rdparty_2tvm_2nnvm_2include_2nnvm_2c__api_8h.html#a830ae32828b06eab796e2f76852591ab',1,'c_api.h']]],
   ['nodeentrymap',['NodeEntryMap',['../namespacennvm.html#a90679a7e880dbf3f009d8d5bfc99c8d4',1,'nnvm']]],
-  ['nodeptr',['NodePtr',['../namespacemxnet.html#a2ab681b45ccee554c37775e5430e237f',1,'mxnet']]]
+  ['nodeptr',['NodePtr',['../namespacemxnet.html#a2ab681b45ccee554c37775e5430e237f',1,'mxnet']]],
+  ['numpydefaultdtype',['NumpyDefaultDtype',['../namespacemxnet.html#a7f4537b321a65b5b33343c522df887f3',1,'mxnet']]]
 ];
